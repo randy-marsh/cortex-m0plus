ecc02040db0ce1b9ace9ff86e54d18868cc6d165
randy-marsh <juanignacio.fraile@edu.uah.es>
2015-09-25 14:02:31 +0200

    cortex-m0plus

---
+++ vtor.c
@@ -0,0 +1,20 @@
+/*
+ * vtor.c
+ *
+ *  Created on: Jul 30, 2015
+ *      Author: juan
+ */
+
+
+void __attribute__((naked))
+vtor(unsigned long addr)
+{
+
+	__asm("    ldr     r1,=0xE000ED08\n"
+		  "    str	   r0,[r1]\n"
+		  "    ldr     sp,[r0]\n"
+		  "    ldr     r0,[r0,#4]\n"
+		  "	   bx 	   r0\n"
+          :  : "r" (addr) : "memory", "r0", "r1");
+
+}
---
+++ vtor.h
@@ -0,0 +1,13 @@
+/*
+ * vtor.h
+ *
+ *  Created on: Jul 30, 2015
+ *      Author: juan
+ */
+
+#ifndef VTOR_H_
+#define VTOR_H_
+
+void vtor(unsigned long addr);
+
+#endif /* VTOR_H_ */
